5a111529dbbc04bee2e284e09ae3914949ce9797
sajid anwar <146831923+Sajidsohil@users.noreply.github.com>
2023-11-28 17:35:32 +0530

    program to print helloword by using new line character .c

---
+++ program to print helloword by using new line character .c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+   // printf() is a standard library function in C, which prints the string inside the parentheses
+   printf("Hello, World!\n");
+
+   return 0;
+}
