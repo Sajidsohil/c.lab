7d1fe876278d3a4a40d9a690b4aaca3eb0f22f28
sajid anwar <146831923+Sajidsohil@users.noreply.github.com>
2023-11-28 17:37:58 +0530

    Create program to print hello world by using tab character .c

---
+++ program to print hello world by using tab character .c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+   // printf() is a standard library function in C, which prints the string inside the parentheses
+   printf("Hello, World!\t");
+
+   return 0;
+}
